c39b96ef465462aae713059a4d92f1c3fcc93df7
LucasSGonza <lucassilva.goncalves@hotmail.com>
2024-10-30 20:25:44 -0300

    feat: adicionar imagem de fundo e novas pequenas add

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,13 @@ import { Box, TextField } from "@mui/material";
 export const HomePage = () => {
   return (
     <Box
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundImage: "url(/src/assets/brasileirao-wallpaper.jpg)",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <h1>Footdle</h1>
       <TextField variant="outlined" placeholder="Digite o nome do jogador" />
